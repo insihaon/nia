e0aec9803ba23c2940dc340bd5452b14b110aaa1
gyujin631 <gyujin631@gmail.com>
2021-08-17 17:34:39 +0900

    1

---
+++ legacy/2021NIA/NiaPingPreprocessorApplication/src/main/java/com/nia/ping/alarm/preprocessor/mapper/AlarmMapper.java
@@ -0,0 +1,19 @@
+package com.nia.ping.alarm.preprocessor.mapper;
+
+import com.nia.ping.alarm.preprocessor.vo.alarm.PingAlarmCntVo;
+import com.nia.ping.alarm.preprocessor.vo.alarm.PingAlarmVo;
+import com.nia.ping.alarm.preprocessor.vo.alarm.PingPolicyVo;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.HashMap;
+
+@Mapper
+public interface AlarmMapper{
+    String selectPingAlarmKey();
+    PingAlarmVo selectPingAlarmCheck(HashMap<String, String> hashMap);
+    PingPolicyVo selectPingPolicy();
+    void insertPingAlarm(PingAlarmVo pingAlarmVo);
+    void insertPingCollect(PingAlarmVo pingAlarmVo);
+    void updatePingAlarmCnt(HashMap<String, String> hashMap);
+    void updatePingAlarmClear(HashMap<String, String> hashMap);
+}
---
+++ legacy/2021NIA/NiaPingPreprocessorApplication/src/main/java/com/nia/ping/alarm/preprocessor/service/NiaAlarmHdlService.java
@@ -0,0 +1,11 @@
+package com.nia.ping.alarm.preprocessor.service;
+
+/**
+
+ * @author
+ *
+ */
+public interface NiaAlarmHdlService {
+
+	void niaAlarmHdlProcessor(String pringData) throws Exception;
+}
