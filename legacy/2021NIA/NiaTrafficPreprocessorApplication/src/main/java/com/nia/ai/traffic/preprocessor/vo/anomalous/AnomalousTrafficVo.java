4ec7c8cef13bfe19b609c3a07af13298fa1ef100
gyujin631 <gyujin631@gmail.com>
2021-12-13 16:28:13 +0900

    연결 정보 검색 로직 수정

--- legacy/2021NIA/NiaTrafficPreprocessorApplication/src/main/java/com/nia/ai/traffic/preprocessor/vo/anomalous/AnomalousTrafficVo.java
+++ legacy/2021NIA/NiaTrafficPreprocessorApplication/src/main/java/com/nia/ai/traffic/preprocessor/vo/anomalous/AnomalousTrafficVo.java
@@ -2,19 +2,21 @@ package com.nia.ai.traffic.preprocessor.vo.anomalous;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Data;
+import lombok.ToString;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 import java.io.Serializable;
 
 @Data
+@ToString
 @Component()
 @Scope(value = "prototype")
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class AnomalousTrafficVo implements Serializable {
 
     private String strifid;
-    private int inttimestamp;
+    private String inttimestamp;
     private int fltbpsin;
     private String fltbpsin_predict;
     private String fltbpsin_threshold_upper;
