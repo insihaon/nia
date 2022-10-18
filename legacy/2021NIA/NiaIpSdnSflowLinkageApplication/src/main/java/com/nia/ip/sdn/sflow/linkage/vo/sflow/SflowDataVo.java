dee4855579bc7f7e61cadb5cb1152e4f9b83332f
gyujin <dddd>
2022-10-18 14:16:59 +0900

    이용기관 연동 추가

---
+++ legacy/2021NIA/NiaIpSdnSflowLinkageApplication/src/main/java/com/nia/ip/sdn/sflow/linkage/vo/sflow/SflowDataVo.java
@@ -0,0 +1,19 @@
+package com.nia.ip.sdn.sflow.linkage.vo.sflow;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Data;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+import java.io.Serializable;
+
+@Component
+@Scope(value = "prototype")
+@Data
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class SflowDataVo implements Serializable {
+    private FieldsVo fields;
+    private String name;
+    private TagsVo tags;
+    private int timestamp;
+}
