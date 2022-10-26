0ff49041d8bf815646472ab5564b8e2514635de4
minyong2 <owow77@naver.com>
2022-10-26 09:07:58 +0900

    Changes

---
+++ EngineNiaServer2021/NiaIpSdnSyslogLinkageApplication/src/main/java/com/nia/ip/sdn/syslog/linkage/listener/NiaSyslogDataMsgListener.java
@@ -0,0 +1,45 @@
+package com.nia.ip.sdn.syslog.linkage.listener;
+
+import com.nia.ip.sdn.syslog.linkage.common.UtlCommon;
+import com.nia.ip.sdn.syslog.linkage.service.IpSdnSyslogService;
+import com.nia.ip.sdn.syslog.linkage.vo.syslog.SyslogDataVo;
+import org.apache.commons.lang3.exception.ExceptionUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.ObjectFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.kafka.annotation.KafkaListener;
+
+public class NiaSyslogDataMsgListener {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(NiaSyslogDataMsgListener.class);
+
+    @Autowired
+    @Qualifier("IpSdnSyslogService")
+    private IpSdnSyslogService ipSdnSyslogService;
+
+    @Autowired
+    private org.springframework.beans.factory.ObjectFactory<SyslogDataVo> pingRowDataVoObjectFactory;
+
+    @KafkaListener(topics = "telegraf-syslog", groupId = "syslogData")
+    public void onMessege(String message){
+        SyslogDataVo syslogDataVo;
+
+        try {
+            LOGGER.info(">>>>>>>>>>[NiaSyslogDataMsgListener] on Message <<<<<<<<<<<<<<<<");
+            Object obj;
+            syslogDataVo = pingRowDataVoObjectFactory.getObject();
+
+            obj = UtlCommon.jsonToObject(syslogDataVo, message);
+            syslogDataVo = (SyslogDataVo) obj;
+
+            ipSdnSyslogService.syslogDataHdlProcessor(syslogDataVo);
+
+        }catch (Exception e){
+            LOGGER.error("=====> [NiaSyslogDataMsgListener] onMessage error "+ ExceptionUtils.getStackTrace(e)+ "<=====");
+
+        }
+    }
+
+}
