8177a656223327cfd20ae61e71e80f64769e1e94
gyujin <dddd>
2022-11-09 15:13:12 +0900

    수정

--- EngineNiaServer2021/NiaIpCountryCodeLinkageApplication/src/main/java/com/nia/ip/country/code/linkage/mapper/SflowMapper.java
+++
@@ -1,9 +0,0 @@
-package com.nia.ip.country.code.linkage.mapper;
-
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface SflowMapper {
-    int selectSflowSeq();
-    void insertSflowData(SflowCollectVo sflowCollectVo);
-}
