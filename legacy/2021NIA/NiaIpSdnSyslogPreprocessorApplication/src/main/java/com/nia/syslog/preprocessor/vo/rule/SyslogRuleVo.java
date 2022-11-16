d3435c332aed0bee67ffec5feb3844d883b87784
gyujin <dddd>
2022-11-16 10:44:33 +0900

    수정

---
+++ legacy/2021NIA/NiaIpSdnSyslogPreprocessorApplication/src/main/java/com/nia/syslog/preprocessor/vo/rule/SyslogRuleVo.java
@@ -0,0 +1,33 @@
+package com.nia.syslog.preprocessor.vo.rule;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+import lombok.*;
+import java.sql.Timestamp;
+import java.io.Serializable;
+
+@Data
+@Component
+@Scope(value = "prototype")
+public class SyslogRuleVo implements Serializable {
+
+	private int syslogRuleId;
+	private String syslogRuleNm;
+	private String syslogContent;
+	private String nodeType;
+	private int priorityOrder;
+	private String alarmSeverity;
+	private String occurStr1;
+	private String occurStr2;
+	private String occurStr3;
+	private String occurExceptStr1;
+	private String occurExceptStr2;
+	private String occurExceptStr3;
+	private String clearStr1;
+	private String clearStr2;
+	private String clearStr3;
+	private String clearExceptStr1;
+	private String clearExceptStr2;
+	private String clearExceptStr3;
+	private String useYn;
+}
