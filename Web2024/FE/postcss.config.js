b8af73c9b0209007d6e8c57f09079483efcf1874
SJinny <sujin33806@gmail.com>
2024-02-05 10:41:40 +0900

    tailwind install 및 setting

--- Web2024/FE/postcss.config.js
+++ Web2024/FE/postcss.config.js
@@ -1,5 +1,6 @@
+const autoprefixer = require('autoprefixer')
+const tailwindcss = require('tailwindcss')
+
 module.exports = {
-  plugins: {
-    autoprefixer: {}
-  }
+  plugins: [tailwindcss, autoprefixer],
 }
