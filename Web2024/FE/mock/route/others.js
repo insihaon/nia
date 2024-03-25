1609dfab05850a67b8d6e8b89a0aa61c2af38651
dimmby <dimmbygo@gmail.com>
2024-03-25 15:14:28 +0900

    client mock 보완 (datahub, json) - 진행중

---
+++ Web2024/FE/mock/route/others.js
@@ -0,0 +1,34 @@
+const { responseJson } = require('../utils')
+
+module.exports = [
+  {
+    url: '/selectList\.*',
+    type: 'post',
+    response: responseJson
+  },
+  {
+    url: '/selectOne\.*',
+    type: 'post',
+    response: responseJson
+  },
+  {
+    url: '/modify\.*',
+    type: 'post',
+    response: responseJson
+  },
+  {
+    url: '/\.*',
+    type: 'get',
+    response: responseJson
+  },
+  {
+    url: '/\.*',
+    type: 'post',
+    response: responseJson
+  },
+  {
+    url: '/\.*',
+    type: 'delete',
+    response: responseJson
+  }
+]
