9d6e02150332d9fdcd1be511bfed951600bf2552
daxnee <das03041@gmail.com>
2024-03-29 17:06:39 +0900

    [nia] 이용기관 등록 모달 레이아웃 , 기능 구현

--- Web2024/FE/src/views-nia/modal/ModalAddTrafficData.vue
+++ Web2024/FE/src/views-nia/modal/ModalAddTrafficData.vue
@@ -22,8 +22,10 @@
         </span>
         <table class="basic">
           <th>{{ titleNameA }}</th>
-          <td v-if="viewType === 'editAgency'" class="disable">
-            <el-select v-model="rowInfo.nren_name">
+          <td v-if="viewType === 'AGENCY'">
+            <el-select
+              v-model="agency_id"
+            >
               <el-option
                 v-for="item in agencyList"
                 :key="item.value"
@@ -32,27 +34,18 @@
               />
             </el-select>
           </td>
-          <td v-if="viewType === 'editApp'" class="disable">
-            <el-input v-model="rowInfo.protocol">
-              {{ rowInfo.protocol }}
-            </el-input>
+          <td v-if="viewType === 'APP'">
+            <el-input v-model="protocol" />
           </td>
           <tr>
             <th>{{ titleNameB }}</th>
-            <td v-if="viewType === 'editAgency'">
-              <el-input v-model="rowInfo.nren_ip">
-                {{ rowInfo.nren_ip }}
-              </el-input>
-            </td>
-            <td v-if="viewType === 'editApp'">
-              <el-input v-model="rowInfo.port_num">
-                {{ rowInfo.port_num }}
-              </el-input>
+            <td>
+              <el-input v-model="input_item" />
             </td>
           </tr>
         </table>
         <div slot="footer" class="dialog-footer">
-          <el-button size="medium" @click.native="close()">
+          <el-button size="medium" @click.native="modeChange()">
             {{ '등록' }}
           </el-button>
           <el-button class="exit-btn" size="medium" @click.native="close()">
@@ -68,9 +61,9 @@
 import elDragDialog from '@/directive/el-drag-dialog'
 import { Modal } from '@/min/Modal.min'
 import { mapState } from 'vuex'
-import { apiSelectAgencyCodeList } from '@/api/nia'
+import { apiInsertAgencyIpList, apiSelectAgencyCodeList } from '@/api/nia'
 
-const routeName = 'ModalEditTrafficData'
+const routeName = 'ModalAddTrafficData'
 
 export default {
   name: routeName,
@@ -81,8 +74,12 @@ export default {
       name: routeName,
       src: `webpack:///${__filename.replace(/\\/g, '/').replace(/\?.*$/, '')}`,
       viewType: '',
-      rowInfo: {},
-      agencyList: []
+      rowInfo: {
+      },
+      agency_id: '',
+      agencyList: [],
+      protocol: '',
+      input_item: ''
     }
   },
   computed: {
@@ -91,21 +88,21 @@ export default {
       username: state => state.user.name,
     }),
     titleMode() {
-      if (this.viewType === 'editAgency') {
-        return '이용기관 수정'
+      if (this.viewType === 'AGENCY') {
+        return '이용기관 등록'
       } else {
-        return '어플리케이션 수정'
+        return '어플리케이션 등록'
       }
     },
     titleNameA() {
-      if (this.viewType === 'editAgency') {
+      if (this.viewType === 'AGENCY') {
         return '이용기관'
       } else {
         return '프로토콜'
       }
     },
     titleNameB() {
-      if (this.viewType === 'editAgency') {
+      if (this.viewType === 'AGENCY') {
         return 'IP 주소'
       } else {
         return '포트'
@@ -123,7 +120,7 @@ export default {
     },
     onOpen(model, actionMode) {
       this.viewType = model.type
-      this.rowInfo = this._cloneDeep(model.row)
+      this.rowInfo = {}
       this.onloadAgencyCodeList()
     },
     async onloadAgencyCodeList() {
@@ -136,6 +133,41 @@ export default {
         } finally {
           // this.closeLoading(target)
         }
+    },
+    modeChange() {
+      if (this.viewType === 'AGENCY') {
+        this.insertAgencyIpData()
+      } else {
+        this.insertAppData()
+      }
+    },
+    async insertAgencyIpData() {
+       this.$confirm('이용기관을 등록하시겠습니까?', `이용기관 등록`, {
+          confirmButtonText: '예',
+          cancelButtonText: '아니오',
+          type: 'success'
+        }).then(async() => {
+          const selectedItem = this.agencyList.find(item => item.value === this.agency_id)
+          const param = {
+            nren_id: this.agency_id,
+            nren_name: selectedItem.label,
+            nren_ip: this.input_item
+          }
+          try {
+            const res = await apiInsertAgencyIpList(param)
+            if (res.result > 0) {
+              this.$message('등록 되었습니다.')
+              this.close()
+            }
+          } catch (error) {
+            console.error(error)
+          } finally {
+            /*  */
+          }
+        })
+    },
+    insertAppData() {
+
     },
     onClose() { /* for Override */ },
     onSubmit() {
