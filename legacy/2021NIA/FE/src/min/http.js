6e3e3fc2b94e4d806a4ec4b28831c5eb5a129b23
SJinny <sujin33806@gmail.com>
2024-03-29 09:45:30 +0900

    디버깅 아닐 때 파라미터 decrypt 오류 수정

--- legacy/2021NIA/FE/src/min/http.js
+++ legacy/2021NIA/FE/src/min/http.js
@@ -50,12 +50,12 @@ service.interceptors.request.use(
       config.headers['fileIndex'] = config.fileIndex
       config.headers['project'] = project
       config.headers['urlOrigin'] = config.urlOrigin = url
-      config.headers['jsonfilename'] = getJsonfileName2(url, config)
       if (store.getters.server) {
         config.headers['_t'] = Encrypt.toEncrypt(String(Date.now() - (store.getters.server.timeDiff || 0)))
       }
 
       if (mock === 'BE') {
+        config.headers['jsonfilename'] = getJsonfileName2(url, config)
         config.url = '/mock'
       }
 
