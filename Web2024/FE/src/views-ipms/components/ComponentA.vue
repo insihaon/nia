e94d301427eeb65b68066d68312f311e1a446d91
daxnee <das03041@gmail.com>
2024-06-10 14:30:15 +0900

    [ipms] 컴포넌트 테스트 임시 커밋

---
+++ Web2024/FE/src/views-ipms/components/ComponentA.vue
@@ -0,0 +1,54 @@
+<template>
+  <div :class="{ [name]: true }">
+    <el-select
+      v-model="searchSelectModel"
+      collapse-tags
+      filterable
+      clearable
+      size="mini"
+      :placeholder="item.placeholder"
+      reserve-keyword
+      remote
+    >
+      <el-option
+        v-for="(option, i) in item.options"
+        :key="i"
+        :label="option.label"
+        :value="option.value"
+      />
+    </el-select>
+  </div>
+</template>
+<script>
+import { Base } from '@/min/Base.min'
+
+const routeName = 'ComponentA'
+export default {
+  name: routeName,
+  extends: Base,
+  props: {
+  },
+  data() {
+    return {
+      name: routeName,
+      src: `webpack:///${__filename.replace(/\\/g, '/').replace(/\?.*$/, '')}`,
+      searchSelectModel: '',
+      options: [
+        { label: '공인', value: 1 },
+        { label: 'Bogon', value: 2 },
+        { label: '유/무선공용', value: 3 }
+      ]
+    }
+  },
+  methods: {
+  }
+}
+</script>
+<style lang="scss" scoped>
+.ComponentA {
+  height: auto;
+  ::v-deep .el-range-editor.el-input__inner {
+    width: 100%;
+  }
+}
+</style>
---
+++ Web2024/FE/src/views-ipms/components/ComponentC.vue
@@ -0,0 +1,33 @@
+<template>
+  <div :class="{ [name]: true }">
+    <el-input v-model="searchInputModel" size="mini" />
+  </div>
+</template>
+<script>
+import { Base } from '@/min/Base.min'
+
+const routeName = 'ComponentC'
+export default {
+  name: routeName,
+  extends: Base,
+  props: {
+  },
+  data() {
+    return {
+      name: routeName,
+      src: `webpack:///${__filename.replace(/\\/g, '/').replace(/\?.*$/, '')}`,
+      searchInputModel: ''
+    }
+  },
+  methods: {
+  }
+}
+</script>
+<style lang="scss" scoped>
+.ComponentC {
+  height: auto;
+  ::v-deep .el-range-editor.el-input__inner {
+    width: 100%;
+  }
+}
+</style>
