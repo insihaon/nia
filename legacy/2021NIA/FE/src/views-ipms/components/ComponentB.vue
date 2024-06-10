63f6753465ab6e65357de7cb24abbf3a1876d466
daxnee <das03041@gmail.com>
2024-06-10 14:30:15 +0900

    [ipms] 컴포넌트 테스트 임시 커밋

---
+++ legacy/2021NIA/FE/src/views-ipms/components/ComponentB.vue
@@ -0,0 +1,41 @@
+<template>
+  <div :class="{ [name]: true }">
+    <el-date-picker
+      v-model="searchtimeModel"
+      type="daterange"
+      size="mini"
+      start-placeholder="시작일"
+      end-placeholder="종료일"
+    />
+  </div>
+</template>
+<script>
+import { Base } from '@/min/Base.min'
+
+const routeName = 'ComponentB'
+export default {
+  name: routeName,
+  extends: Base,
+  props: {
+
+  },
+  data() {
+    return {
+      name: routeName,
+      src: `webpack:///${__filename.replace(/\\/g, '/').replace(/\?.*$/, '')}`,
+      searchtimeModel: []
+    }
+  },
+  methods: {
+
+  }
+}
+</script>
+<style lang="scss" scoped>
+.ComponentA {
+  height: auto;
+  ::v-deep .el-range-editor.el-input__inner {
+    width: 100%;
+  }
+}
+</style>
