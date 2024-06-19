3bbf99edd8dd7c87656e2a3b567b6f4ba5a8a738
SJinny <sujin33806@gmail.com>
2024-06-19 17:10:06 +0900

    [IPMS] isDashboard 옵션 불필요 페이지 삭제

--- legacy/2021NIA/FE/src/views-ipms/menus/ipAllocationMng/ipAllocation.vue
+++ legacy/2021NIA/FE/src/views-ipms/menus/ipAllocationMng/ipAllocation.vue
@@ -1,5 +1,5 @@
 <template>
-  <el-row class="w-100 h-100" :class="{'px-12': !isDashboard}">
+  <el-row class="w-100 h-100">
     <DynamicComponentLoader
       :component-keys="componentList"
       @handle-search="handleSearch"
@@ -33,8 +33,6 @@ export default {
   name: routeName,
   components: { CompTable, DynamicComponentLoader },
   extends: Base,
-  props: {
-  },
   data() {
     return {
       name: routeName,
