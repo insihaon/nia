a006551478449161b8623e70299a5a05d2f1d237
daxnee <das03041@gmail.com>
2024-06-20 17:35:34 +0900

    [ipms] - 상품관리, 사용자관리 페이지 구현 - 조직 검색 모달 추가

---
+++ Web2024/FE/src/views-ipms/menus/operInfoMng/productMng/serviceCodeManagement.vue
@@ -0,0 +1,60 @@
+<template>
+  <el-row class="w-100 h-100">
+    <DynamicComponentLoader
+      :component-keys="componentList"
+      @handle-search="handleSearch"
+    />
+    <el-col :span="24">
+      <compTable
+        :prop-table-height="300"
+        :prop-column="tableColumns"
+        :prop-is-pagination="true"
+        :prop-is-check-box="false"
+        prop-grid-menu-id="inputSpeed"
+        :prop-grid-indx="1"
+      >
+        <template slot="text-description">
+          <span>
+            서비스 코드
+          </span>
+        </template>
+      </compTable>
+    </el-col>
+  </el-row>
+</template>
+<script>
+import { Base } from '@/min/Base.min'
+import DynamicComponentLoader from '@/views-ipms/components/DynamicComponentLoader.vue'
+import CompTable from '@/components/elTable/CompTable.vue'
+const routeName = 'ServiceCodeManagement'
+
+export default {
+  name: routeName,
+  components: { CompTable, DynamicComponentLoader },
+  extends: Base,
+  data() {
+    return {
+      name: routeName,
+      src: `webpack:///${__filename.replace(/\\/g, '/').replace(/\?.*$/, '')}`,
+      tableColumns: [
+        { prop: '', label: '코드', align: 'center', sortable: true, columnVisible: true, showOverflow: true },
+        { prop: '', label: '서비스명', align: 'center', sortable: true, columnVisible: true, showOverflow: true },
+        { prop: '', label: 'NeOSS연동 여부', align: 'center', sortable: true, columnVisible: true, showOverflow: true },
+        { prop: '', label: '비고', align: 'center', sortable: true, columnVisible: true, showOverflow: true },
+        { prop: '', label: '수정', align: 'center', sortable: true, columnVisible: true, showOverflow: true },
+      ],
+      componentList: [
+        { key: 'IncludeYN', props: { label: 'NeOSS연동 여부' } },
+        { key: 'InputType', props: { label: '서비스', propsParameterKey: 'service' } },
+        { key: 'InputType', props: { label: '코드명', propsParameterKey: 'codeNm' } },
+      ]
+    }
+  },
+  methods: {
+    onClickSearch() {
+    }
+  }
+}
+</script>
+<style lang="css" scoped>
+</style>
---
+++ Web2024/FE/src/views-ipms/modal/ModalOperationTeam.vue
@@ -0,0 +1,94 @@
+<template>
+  <div>
+    <el-dialog
+      v-if="animationVisible"
+      v-el-drag-dialog
+      :visible.sync="visible"
+      :width="domElement.maxWidth + `px`"
+      :fullscreen.sync="fullscreen"
+      :modal-append-to-body="false"
+      :append-to-body="true"
+      :modal="modal"
+      :close-on-click-modal="closeOnClickModal"
+      :loading="loading"
+      class="nia-edit-dialog"
+      :class="{ [name]: true }"
+    >
+      <span slot="title">
+        <i class="el-icon-document mr-2" style="font-size: 17px" />
+        운용팀 검색
+        <hr>
+      </span>
+      <el-row class="w-100 h-100">
+        <DynamicComponentLoader
+          :component-keys="componentList"
+          @handle-search="handleSearch"
+        />
+        <el-col :span="24">
+          <compTable :prop-table-height="300" :prop-column="tableColumns" :prop-is-pagination="false" :prop-is-check-box="true" prop-grid-menu-id="inputSpeed" :prop-grid-indx="1">
+            <template slot="text-description">
+              <span>
+                운용팀
+              </span>
+            </template>
+          </compTable>
+        </el-col>
+      </el-row>
+      <div slot="footer" class="dialog-footer">
+        <el-button size="mini" icon="el-icon-edit" @click="onClickData()">선택</el-button>
+        <el-button size="mini" type="info" class="el-icon-close" @click.native="close()">
+          {{ $t('exit') }}
+        </el-button>
+      </div>
+    </el-dialog>
+  </div>
+</template>
+
+<script>
+import elDragDialog from '@/directive/el-drag-dialog'
+import { Modal } from '@/min/Modal.min'
+import CompTable from '@/components/elTable/CompTable.vue'
+import DynamicComponentLoader from '@/views-ipms/components/DynamicComponentLoader.vue'
+
+const routeName = 'ModalOperationTeam'
+
+export default {
+  name: routeName,
+  components: { DynamicComponentLoader, CompTable },
+  directives: { elDragDialog },
+  extends: Modal,
+  data() {
+    return {
+      name: routeName,
+      src: `webpack:///${__filename.replace(/\\/g, '/').replace(/\?.*$/, '')}`,
+      componentList: [
+        //  { key: 'SsvcLineType', props: { label: '분류', lvl: 3, multi: [2] } },
+      ],
+       tableColumns: [
+        { prop: '', label: '선택', align: 'center', sortable: true, columnVisible: true, showOverflow: true },
+        { prop: '', label: '운용조직 ID', align: 'center', sortable: true, columnVisible: true, showOverflow: true },
+        { prop: '', label: '운용조직 명', align: 'center', sortable: true, columnVisible: true, showOverflow: true },
+      ],
+    }
+  },
+  methods: {
+    onCreated() {
+      Modal.methods.onCreated.call(this)
+      this.closeOnClickModal = false
+      this.domElement.maxWidth = 700
+    },
+    onOpen(model, actionMode) {
+    },
+    onClickData() {
+    },
+    onClose() {
+    },
+    handleSearch(requestParameter) {
+      console.log(requestParameter)
+    }
+  },
+}
+</script>
+<style lang="scss" scoped>
+@import '~@/styles/variables.scss';
+</style>
