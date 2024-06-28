8844472b7f28f94f7a989ef34fbbb6c45315c450
daxnee <das03041@gmail.com>
2024-06-28 16:46:26 +0900

    [ipms] - 수용국 전체 옵션 추가, 디폴트 설정 - 배치연동정보, 우편번호 연동관리, iFoms 사설정보, Whois 정보공개 페이지 구현

---
+++ legacy/2021NIA/FE/src/views-ipms/menus/operInfoMng/whoIs/whoisInfoOpenMng.vue
@@ -0,0 +1,128 @@
+<template>
+
+  <el-row class="w-100 h-100">
+    <div class="content_result my-2">
+      <table class="tbl_data">
+        <tbody>
+          <tr class="top last">
+            <template v-for="(item, index) in tableItems">
+              <th :key="'th-' + index">{{ item.title }}</th>
+              <td :key="'td-' + index">{{ item.value }} 건</td>
+            </template>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+
+    <DynamicComponentLoader
+      :component-keys="componentList"
+      @handle-search="handleSearch"
+    />
+    <el-col :span="24">
+      <compTable
+        :prop-table-height="'calc(100% - 80px)'"
+        :prop-column="tableColumns"
+        :prop-is-pagination="true"
+        :prop-is-check-box="true"
+        prop-grid-menu-id="inputSpeed"
+        :prop-grid-indx="1"
+      >
+        <template slot="text-description">
+          <span>
+            조회 결과
+          </span>
+        </template>
+      </compTable>
+    </el-col>
+  </el-row>
+
+</template>
+<script>
+import { Base } from '@/min/Base.min'
+import DynamicComponentLoader from '@/views-ipms/components/DynamicComponentLoader.vue'
+import CompTable from '@/components/elTable/CompTable.vue'
+const routeName = 'WhoisInfoOpenManagement'
+
+export default {
+  name: routeName,
+  components: { CompTable, DynamicComponentLoader },
+  extends: Base,
+  data() {
+    return {
+      name: routeName,
+      src: `webpack:///${__filename.replace(/\\/g, '/').replace(/\?.*$/, '')}`,
+        tableColumns: [
+          { prop: '', label: 'No', align: 'center', columnVisible: true, showOverflow: true },
+          { prop: '', label: '시작 IP', align: 'center', columnVisible: true, showOverflow: true },
+          { prop: '', label: '마지막 IP', align: 'center', columnVisible: true, showOverflow: true },
+          { prop: '', label: '노드', align: 'center', columnVisible: true, showOverflow: true },
+          { prop: '', label: '서비스', align: 'center', columnVisible: true, showOverflow: true },
+          { prop: '', label: '기관명', align: 'center', columnVisible: true, showOverflow: true },
+          { prop: '', label: '네트워크이름', align: 'center', columnVisible: true, showOverflow: true },
+          { prop: '', label: '작업종류', align: 'center', columnVisible: true, showOverflow: true },
+          { prop: '', label: '변경일시', align: 'center', columnVisible: true, showOverflow: true },
+          { prop: '', label: '등록현황', align: 'center', columnVisible: true, showOverflow: true },
+          { prop: '', label: '입력구분', align: 'center', columnVisible: true, showOverflow: true },
+        ],
+      componentList: [
+        { key: 'SsvcLineType', props: { label: '계위 정보', lvl: 3 } },
+        { key: 'InputType', props: { label: '사용기관명', prop_parameterKey: 'shostIp' } },
+         {
+          key: 'ApplyStatus', props: {
+          label: '등록현황',
+          prop_parameterKey: 'RboardProgress',
+          prop_options: [
+              { label: '전체', value: '' },
+              { label: 'WHOIS 전송 대기(APP-Kmic간 통신오류)', value: 'RES001' },
+              { label: 'WHOIS 전송(Kmic-Whois 간 통신오류)', value: 'RES002' },
+              { label: 'WHOIS 반송', value: 'RES003' },
+              { label: 'WHOIS 등록완료', value: 'RES004' },
+              { label: 'WHOIS 전송완료(Whois 응답 수신오류)', value: 'RES005' },
+            ]
+          }
+        },
+        { key: 'ServiceOrg', props: { isMulti: false, sortTypeDefaultVal: 'ALL' } },
+        { key: 'InputType', props: { label: 'IP 주소', prop_parameterKey: 'shostIp' } },
+        {
+          key: 'ApplyStatus', props: {
+          label: '작업종류',
+          prop_parameterKey: 'RboardProgress',
+          prop_options: [
+              { label: '전체', value: '' },
+              { label: '신규 신청서', value: 'RES001' },
+              { label: '추가 신청서', value: 'RES002' },
+              { label: '삭제 신청서', value: 'RES003' },
+              { label: '변경 신청서', value: 'RES004' },
+            ]
+          }
+        },
+        { key: 'DateRange', props: { label: '변경일' } },
+        { key: 'InputType', props: { label: 'I/F명', prop_parameterKey: 'sifNm' } },
+        { key: 'InputType', props: { label: 'I/F IP', prop_parameterKey: 'sifIp' } },
+        { key: 'SortType', props: {
+            sortTypeDefaultVal: 'DAPPLY_DT',
+            label: '정렬', prop_options: [
+              { label: '장비IP', value: 'DAPPLY_DT' },
+              { label: 'I/F명', value: 'SIF_NM' },
+            ]
+          }
+        },
+      ],
+      tableItems: [
+        { title: '전송대기', value: '275' },
+        { title: '전송', value: '13' },
+        { title: '전송완료', value: '0' },
+        { title: '반송', value: '9' },
+        { title: '등록완료', value: '287091' }
+      ]
+    }
+  },
+  methods: {
+      handleSearch(requestParameter) {
+      console.log(requestParameter)
+    }
+  }
+}
+</script>
+<style lang="css" scoped>
+</style>
