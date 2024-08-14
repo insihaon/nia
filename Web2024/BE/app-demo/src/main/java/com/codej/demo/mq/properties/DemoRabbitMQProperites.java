96b467c61fb70f878cfc436e33bab1c123e4d1d0
dimmby <dimmbygo@gmail.com>
2024-08-14 11:14:32 +0900

    app-demo 추가

---
+++ Web2024/BE/app-demo/src/main/java/com/codej/demo/mq/properties/DemoRabbitMQProperites.java
@@ -0,0 +1,44 @@
+package com.codej.demo.mq.properties;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import com.codej.base.dto.AppDto;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import lombok.Data;
+
+@Component
+@Data
+public class DemoRabbitMQProperites {
+
+    @Autowired
+    AppDto appDto;
+
+    @Value("${spring.rabbitmq.embedded-server-start:false}")
+    private Boolean embeddedEnabled;
+
+    @Value("${spring.rabbitmq.enabled:true}")
+    private Boolean enabled; // false: rabbitmq 를 사용 하지 않는다
+
+    @Value("${spring.rabbitmq.address:incodej-lab.iptime.org}")
+    private String address;
+
+    @Value("${spring.rabbitmq.port:6786}")
+    private int port;
+
+    @Value("${spring.rabbitmq.username:admin}")
+    private String userName;
+
+    @JsonIgnore
+    @Value("${spring.rabbitmq.password:admin12#$}")
+    private String password;
+
+    @Value("${spring.rabbitmq.UiToEngine:UiToEngineIndexDirectly}")
+    private String uiToEngine;
+
+    @Value("${spring.rabbitmq.EngineToUi:EngineToUiIndexDirectly}")
+    private String engineToUi;
+
+}
