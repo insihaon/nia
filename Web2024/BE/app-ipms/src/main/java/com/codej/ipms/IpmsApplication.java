b259332f9345c93f0ce34e0e8668c46b9c78216e
dimmby <dimmbygo@gmail.com>
2024-08-14 12:24:23 +0900

    app-ipms 추가

---
+++ Web2024/BE/app-ipms/src/main/java/com/codej/ipms/IpmsApplication.java
@@ -0,0 +1,36 @@
+package com.codej.ipms;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@SpringBootApplication(scanBasePackages = {"com.codej"}, exclude = { SecurityAutoConfiguration.class })
+@EnableCaching
+@EnableWebSecurity
+@EnableAsync
+@EnableScheduling
+public class IpmsApplication extends SpringBootServletInitializer implements WebMvcConfigurer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(IpmsApplication.class);
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(IpmsApplication.class, args);
+		log.info("\n\n>>> {} {} started <<<\n", IpmsApplication.class.getSimpleName(), new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date()));
+	}
+}
