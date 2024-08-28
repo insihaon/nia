1db8dab767d47ea1581b518e720432e30d2777aa
daxnee <das03041@gmail.com>
2024-08-28 17:56:29 +0900

    [ipms] - 사설AS할당/반납 신청 , whois정보변경 신청 조회/상세/ 등록 모달 구현, 관련 상세 기능 구현

--- Web2024/FE/src/views-ipms/menus/board/whoisInfoChange.vue
+++ Web2024/FE/src/views-ipms/menus/board/whoisInfoChange.vue
@@ -3,23 +3,32 @@
     <DynamicComponentLoader
       ref="searchCondition"
       :component-keys="componentList"
-      @handle-search="handleSearch"
+      @handle-search="fnViewListWhoisModReq"
     />
     <el-col ref="tableContainer" :span="24">
       <compTable
         :prop-table-height="'calc(100% - 80px)'"
         :prop-column="tableColumns"
+        :prop-data="resultListVo"
         :prop-is-pagination="false"
         :prop-is-check-box="false"
         prop-grid-menu-id="inputSpeed"
         :prop-grid-indx="1"
+        :prop-on-click="onClcikRow"
       >
         <template slot="text-description">
           <span>
             WHOIS 정보 변경 신청 조회결과
           </span>
         </template>
+        <template slot="add-features">
+          <div class="float-right">
+            <el-button size="mini" icon="el-icon-document-add" @click="fnViewDetailWhoisMod('', 'create')">신청</el-button>
+          </div>
+        </template>
       </compTable>
+      <ModalDetailWhoisMod ref="ModalDetailWhoisMod" />
+      <ModalRegWhoisModReq ref="ModalRegWhoisModReq" />
     </el-col>
   </el-row>
 </template>
@@ -28,12 +37,15 @@ import { Base } from '@/min/Base.min'
 import CompTable from '@/components/elTable/CompTable.vue'
 import DynamicComponentLoader from '@/views-ipms/components/DynamicComponentLoader.vue'
 import tableHeightMixin from '@/mixin/tableHeightMixin'
+import ModalDetailWhoisMod from '@/views-ipms/modal/notice/ModalDetailWhoisMod.vue'
+import ModalRegWhoisModReq from '@/views-ipms/modal/notice/ModalRegWhoisModReq.vue'
+import { ipmsModelApis, apiRequestModel } from '@/api/ipms'
 
 const routeName = 'WhoisInfoChange'
 
 export default {
   name: routeName,
-  components: { CompTable, DynamicComponentLoader },
+  components: { CompTable, DynamicComponentLoader, ModalDetailWhoisMod, ModalRegWhoisModReq },
   extends: Base,
   mixins: [tableHeightMixin],
   data() {
@@ -41,15 +53,15 @@ export default {
       name: routeName,
       src: `webpack:///${__filename.replace(/\\/g, '/').replace(/\?.*$/, '')}`,
       tableColumns: [
-        { prop: '', label: '신청번호', align: 'center', sortable: true, columnVisible: true, showOverflow: true },
-        { prop: '', label: '시작IP', align: 'center', sortable: true, columnVisible: true, showOverflow: true },
-        { prop: '', label: '끝IP', align: 'center', sortable: true, columnVisible: true, showOverflow: true },
-        { prop: '', label: '기관명(변경전)', align: 'center', sortable: true, columnVisible: true, showOverflow: true },
-        { prop: '', label: '기관명(변경후)', align: 'center', sortable: true, columnVisible: true, showOverflow: true },
-        { prop: '', label: '신청자', align: 'center', sortable: true, columnVisible: true, showOverflow: true },
-        { prop: '', label: '신청일시', align: 'center', sortable: true, columnVisible: true, showOverflow: true },
-        { prop: '', label: '처리일시', align: 'center', sortable: true, columnVisible: true, showOverflow: true },
-        { prop: '', label: '상태', align: 'center', sortable: true, columnVisible: true, showOverflow: true },
+        { prop: 'nmodify_apply_seq', label: '신청번호', align: 'center', sortable: true, columnVisible: true, showOverflow: true },
+        { prop: 'sfirstAddr', label: '시작IP', align: 'center', sortable: true, columnVisible: true, showOverflow: true },
+        { prop: 'slastAddr', label: '끝IP', align: 'center', sortable: true, columnVisible: true, showOverflow: true },
+        { prop: 'sBefOrgName', label: '기관명(변경전)', align: 'center', sortable: true, columnVisible: true, showOverflow: true },
+        { prop: 'sAftOrgName', label: '기관명(변경후)', align: 'center', sortable: true, columnVisible: true, showOverflow: true },
+        { prop: 'sApplyNm', label: '신청자', align: 'center', sortable: true, columnVisible: true, showOverflow: true },
+        { prop: 'dApplyDt', label: '신청일시', align: 'center', sortable: true, columnVisible: true, showOverflow: true },
+        { prop: 'dApprovalDt', label: '처리일시', align: 'center', sortable: true, columnVisible: true, showOverflow: true },
+        { prop: 'sStatNm', label: '상태', align: 'center', sortable: true, columnVisible: true, showOverflow: true },
       ],
       componentList: [
         { key: 'IpAddress', props: { isShowSelectBox: false } },
@@ -75,11 +87,41 @@ export default {
           }
         },
       ],
+      resultListVo: []
     }
   },
+  mounted() {
+    this.fnViewListWhoisModReq()
+  },
   methods: {
-    handleSearch(requestParameter) {
-      console.log(requestParameter)
+   async fnViewListWhoisModReq(requestParameter) {
+      try {
+        const res = await apiRequestModel(ipmsModelApis.viewListWhoisModReq, requestParameter)
+        this.resultListVo = res?.result.data
+      } catch (error) {
+        console.error(error)
+      }
+    },
+    onClcikRow(row, type) {
+      this.fnViewDetailWhoisMod(row, 'detail')
+    },
+    async fnViewDetailWhoisMod(row, type) {
+      if (type === 'detail') {
+        try {
+          if (row.nmodify_apply_seq === '' || row.nmodify_apply_seq === null) {
+            return
+          }
+            const tbWhoisModifyVo = {
+              nmodify_apply_seq: row.nmodify_apply_seq
+            }
+            const res = await apiRequestModel(ipmsModelApis.viewDetailWhoisModReq, tbWhoisModifyVo)
+            this.$refs.ModalDetailWhoisMod.open({ row: res.result.data, type: type })
+          } catch (error) {
+            console.error(error)
+        }
+      } else {
+        this.$refs.ModalRegWhoisModReq.open()
+      }
     }
   },
 }
