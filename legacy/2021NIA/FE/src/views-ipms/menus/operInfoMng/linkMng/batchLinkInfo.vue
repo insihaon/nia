a83954f4f2405e9546abf5f33ac30595b914fbf6
SJinny <sujin33806@gmail.com>
2024-09-04 09:27:10 +0900

    [IPMS] 운용정보관리 > 배치연동정보/ 우편번호 연동관리

--- legacy/2021NIA/FE/src/views-ipms/menus/operInfoMng/linkMng/batchLinkInfo.vue
+++ legacy/2021NIA/FE/src/views-ipms/menus/operInfoMng/linkMng/batchLinkInfo.vue
@@ -1,17 +1,21 @@
 <template>
-  <el-row class="w-100 h-100">
+  <el-row ref="container" class="w-100 h-100">
     <DynamicComponentLoader
+      ref="searchCondition"
       :component-keys="componentList"
-      @handle-search="handleSearch"
+      @handle-search="fnViewListTbBatchSvcBas"
     />
-    <el-col :span="24">
+    <el-col ref="tableContainer" :span="24">
       <compTable
+        ref="compTable"
         :prop-table-height="'calc(100% - 80px)'"
+        :prop-data="tableDatas"
         :prop-column="tableColumns"
         :prop-is-pagination="true"
         :prop-is-check-box="false"
         prop-grid-menu-id="inputSpeed"
         :prop-grid-indx="1"
+        :prop-on-click="handleClickCell"
       >
         <template slot="text-description">
           <span>
@@ -20,28 +24,33 @@
         </template>
       </compTable>
     </el-col>
+    <ModalTbBatchSvcBasDetail ref="ModalTbBatchSvcBasDetail" />
   </el-row>
-
 </template>
 <script>
 import { Base } from '@/min/Base.min'
 import DynamicComponentLoader from '@/views-ipms/components/DynamicComponentLoader.vue'
+import ModalTbBatchSvcBasDetail from '@/views-ipms/modal/interlink/ModalTbBatchSvcBasDetail.vue'
 import CompTable from '@/components/elTable/CompTable.vue'
+import tableHeightMixin from '@/mixin/tableHeightMixin'
+import { ipmsModelApis, apiRequestModel, ipmsJsonApis, apiRequestJson } from '@/api/ipms'
+
 const routeName = 'BatchLinkInformation'
 
 export default {
   name: routeName,
-  components: { CompTable, DynamicComponentLoader },
+  components: { CompTable, DynamicComponentLoader, ModalTbBatchSvcBasDetail },
   extends: Base,
+  mixins: [tableHeightMixin],
   data() {
     return {
       name: routeName,
       src: `webpack:///${__filename.replace(/\\/g, '/').replace(/\?.*$/, '')}`,
         tableColumns: [
-          { prop: '', label: '연동 ID', align: 'center', columnVisible: true, showOverflow: true },
-          { prop: '', label: '시스템 명', align: 'center', columnVisible: true, showOverflow: true },
-          { prop: '', label: '연동 형태', align: 'center', columnVisible: true, showOverflow: true },
-          { prop: '', label: '사용여부', align: 'center', columnVisible: true, showOverflow: true },
+          { prop: 'sifId', label: '연동 ID', align: 'center', columnVisible: true, showOverflow: true },
+          { prop: 'ssystemNm', label: '시스템 명', align: 'center', columnVisible: true, showOverflow: true },
+          { prop: 'linktype', label: '연동 형태', align: 'center', columnVisible: true, showOverflow: true },
+          { prop: 'sopstate', label: '사용여부', align: 'center', columnVisible: true, showOverflow: true },
         ],
       componentList: [
         {
@@ -54,13 +63,27 @@ export default {
             ]
           }
         }
-      ]
+      ],
+      tableDatas: [],
     }
   },
+  // 수정: updateTbBatchSvcBas
+  mounted () {
+    this.fnViewListTbBatchSvcBas()
+  },
   methods: {
-      handleSearch(requestParameter) {
-      console.log(requestParameter)
-    }
+    async fnViewListTbBatchSvcBas(requestParameter) {
+      const params = requestParameter ?? this.$refs?.searchCondition?.requestParameter
+      try {
+        const res = await apiRequestModel(ipmsModelApis.viewListTbBatchSvcBas, params)
+        this.tableDatas = res.result.data
+      } catch (error) {
+        this.error(error)
+      }
+    },
+    handleClickCell(row) {
+      this.$refs.ModalTbBatchSvcBasDetail.open({ viewType: 'mobile', fnType: 'update', row })
+    },
   }
 }
 </script>
--- legacy/2021NIA/FE/src/views-ipms/menus/operInfoMng/linkMng/zipCodeLinkMng.vue
+++ legacy/2021NIA/FE/src/views-ipms/menus/operInfoMng/linkMng/zipCodeLinkMng.vue
@@ -1,6 +1,6 @@
 <template>
-  <el-row class="w-100 h-100">
-    <div class="optionBox">
+  <el-row ref="container" class="w-100 h-100">
+    <div ref="searchCondition" class="optionBox">
       <el-row class="optionRow">
         <el-col class="d-flex" :span="12">
           <label>Upload 성공 여부</label>
@@ -34,7 +34,7 @@
       </el-row>
       <el-row>
         <el-col :span="24" align="center" class="searchBtnGroup">
-          <el-button class="btn-r" type="info" size="mini" icon="el-icon-search" @click="handleSearch()">
+          <el-button class="btn-r" type="info" size="mini" icon="el-icon-search" @click="fnUploadView()">
             조회
           </el-button>
           <el-button class="btn-r" type="info" size="mini" icon="el-icon-refresh">
@@ -43,26 +43,28 @@
           <slot name="add-function" />
         </el-col>
       </el-row>
+      <div class="content_result my-2">
+        <div class="section_tit">
+          <span>Upload</span>
+          <em> (업로드시 5분 이상 소요될 수 있습니다.) </em> </div>
+        <table class="tbl_list">
+          <thead>
+            <tr>
+              <th class="first" style="width: 20%">파일 Upload</th>
+              <th scope="col" style="text-align : left; background : none">
+                <input type="file" size="500" @change="handleFileUpload">
+                <el-button class="float-right" type="primary" size="mini" @click="uploadAjax">Upload</el-button>
+              </th>
+            </tr>
+          </thead>
+        </table>
+      </div>
     </div>
-    <div class="content_result my-2">
-      <div class="section_tit">
-        <span>Upload</span>
-        <em> (업로드시 5분 이상 소요될 수 있습니다.) </em> </div>
-      <table class="tbl_list">
-        <thead>
-          <tr>
-            <th class="first" style="width: 20%">파일 Upload</th>
-            <th scope="col" style="text-align : left; background : none">
-              <input type="file" size="500">
-              <el-button class="float-right"> Upload</el-button>
-            </th>
-          </tr>
-        </thead>
-      </table>
-    </div>
-    <el-col :span="24">
+    <el-col ref="tableContainer" :span="24">
       <compTable
+        ref="compTable"
         :prop-table-height="'calc(100% - 80px)'"
+        :prop-data="tableDatas"
         :prop-column="tableColumns"
         :prop-is-pagination="true"
         :prop-is-check-box="true"
@@ -82,34 +84,65 @@
 <script>
 import { Base } from '@/min/Base.min'
 import CompTable from '@/components/elTable/CompTable.vue'
+import tableHeightMixin from '@/mixin/tableHeightMixin'
+import { ipmsModelApis, apiRequestModel, ipmsJsonApis, apiRequestJson } from '@/api/ipms'
+import { onMessagePopup } from '@/utils'
+
 const routeName = 'ZipCodeLinkMng'
 
 export default {
   name: routeName,
   components: { CompTable },
   extends: Base,
+  mixins: [tableHeightMixin],
   data() {
     return {
       name: routeName,
       src: `webpack:///${__filename.replace(/\\/g, '/').replace(/\?.*$/, '')}`,
         succVal: '',
         dateVal: [],
+        file: null,
         tableColumns: [
-          { prop: '', label: 'Upload 파일명', align: 'center', columnVisible: true, showOverflow: true },
-          { prop: '', label: 'Upload 일자', align: 'center', columnVisible: true, showOverflow: true },
-          { prop: '', label: '등록자', align: 'center', columnVisible: true, showOverflow: true },
-          { prop: '', label: 'Upload 성공 여부', align: 'center', columnVisible: true, showOverflow: true },
+          { prop: 'rowNo', label: 'No', width: 50, align: 'center', columnVisible: true, showOverflow: true },
+          { prop: 'file_name', label: 'Upload 파일명', align: 'center', columnVisible: true, showOverflow: true },
+          { prop: '', label: 'Upload 일자', align: 'center', columnVisible: true, showOverflow: true, formatter: (row) => { return row.regist_date ? this.moment(row.regist_date).format('YYYY-MM-DD HH:mm:ss') : '' } },
+          { prop: 'regist_id', label: '등록자', align: 'center', columnVisible: true, showOverflow: true },
+          { prop: 'status', label: 'Upload 성공 여부', align: 'center', columnVisible: true, showOverflow: true },
         ],
+        tableDatas: [],
     }
   },
+  mounted () {
+    this.fnUploadView()
+  },
   methods: {
-    handleSearch() {
+    async fnUploadView() {
       const [searchBgDe, searchEndDe] = this.dateVal
       const param = { status: this.succVal, searchBgDe: searchBgDe ? this.moment(searchBgDe).format('YYYY-MM-DD') : '', searchEndDe: searchEndDe ? this.moment(searchEndDe).format('YYYY-MM-DD') : '' }
-      console.log(param)
-      /*
-      const res = await api(param)
-      */
+      try {
+        const res = await apiRequestModel(ipmsModelApis.uploadView, param)
+        this.tableDatas = res?.result?.data ?? []
+      } catch (error) {
+        this.error(error)
+      }
+    },
+    async uploadAjax() {
+      if (this.file === null) {
+        onMessagePopup(this, '선택된 파일이 없습니다.')
+        return
+      }
+      try {
+        // opermgmt/uploadmgmt/upload.ajax
+        const res = await apiRequestJson(ipmsJsonApis.upload, { file: this.file })
+        if (res.commonMsg === 'SUCCESS') {
+          this.fnUploadView()
+        }
+      } catch (error) {
+        this.error(error)
+      }
+    },
+    handleFileUpload(e) {
+      this.file = e.target.files[0]
     }
   }
 }
