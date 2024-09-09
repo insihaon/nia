d8b29fba2ef7ee77977935156be64cbcb8613efb
dimmby <dimmbygo@gmail.com>
2024-09-09 14:22:08 +0900

    AjpConfig

---
+++ Web2024/BE/app-ipms/src/main/java/com/kt/ipms/config/AjpConfiguration.java
@@ -0,0 +1,30 @@
+package com.kt.ipms.config;
+
+import org.apache.catalina.connector.Connector;
+import org.apache.coyote.ajp.AbstractAjpProtocol;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AjpConfiguration {
+
+    @Bean
+    public WebServerFactoryCustomizer<TomcatServletWebServerFactory> customizer() {
+        return factory -> {
+            Connector ajpConnector = new Connector("AJP/1.3");
+            ajpConnector.setPort(8009);
+            ajpConnector.setSecure(false);
+            ajpConnector.setAllowTrace(false);
+            ajpConnector.setScheme("http");
+
+            // Configure the AJP protocol
+            AbstractAjpProtocol<?> protocol = (AbstractAjpProtocol<?>) ajpConnector.getProtocolHandler();
+            protocol.setSecretRequired(true);
+            protocol.setSecret("ajp-secret-value"); 
+
+            factory.addAdditionalTomcatConnectors(ajpConnector);
+        };
+    }
+}
