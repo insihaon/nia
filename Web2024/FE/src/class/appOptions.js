3bcd561c023b1bb70d7faa813bfb07373755bc0c
daxnee <das03041@gmail.com>
2024-10-04 12:38:07 +0900

    mock mode 원복

--- Web2024/FE/src/class/appOptions.js
+++ Web2024/FE/src/class/appOptions.js
@@ -28,7 +28,7 @@ export class AppOptions extends Storage {
       isOnlyFront: this.readEnv(process.env.VUE_APP_ONLY_FE, false),
       dark: false,
       mobile: Device.instance.mobile ?? false,
-      mock: 'FE',
+      mock: null,
       projectList: ['datahub', 'demo', 'nia', 'ipms'],
       project: APP_PROJECT?.toLowerCase(),
       baseURL: null,
