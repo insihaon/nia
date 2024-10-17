6a979d9caaaf848ab0767809ec3d593778dc58c8
dimmby <dimmbygo@gmail.com>
2024-10-18 08:03:14 +0900

    publishing ipms 배치파일 추가

--- Web2024/FE/publishing/ipms/_run.bat
+++ Web2024/FE/publishing/ipms/_run.bat
@@ -3,24 +3,6 @@ chcp 65001 >nul
 echo 한글이 포함된 경로는 에러가 발생할 수 있습니다.
 echo ================================================
 
-:: npm 캐시 정리
-call npm cache clean --force
-if %ERRORLEVEL% neq 0 (
-    echo "npm cache clean 중 에러가 발생했지만, 계속 진행합니다."
-)
-
-:: package-lock.json 삭제
-call del package-lock.json
-if %ERRORLEVEL% neq 0 (
-    echo "package-lock.json 삭제 중 에러가 발생했지만, 계속 진행합니다."
-)
-
-:: npm 패키지 설치
-call npm install
-if %ERRORLEVEL% neq 0 (
-    echo "npm install 중 에러가 발생했지만, 계속 진행합니다."
-)
-
 :: 개발 서버 실행
 call npm run dev
 if %ERRORLEVEL% neq 0 (
