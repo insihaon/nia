e5ac772bc5ad522f6d0a629f8a51516abd81e1c6
SJinny <sujin33806@gmail.com>
2024-10-22 16:15:18 +0900

    [NIA] 서버모니터링url 프로퍼티 적용

--- legacy/2021NIA/FE/src/views-nia/operationStatusScreen/serverMonitoring.vue
+++ legacy/2021NIA/FE/src/views-nia/operationStatusScreen/serverMonitoring.vue
@@ -91,7 +91,6 @@ import CompInquiryPannel from '@/views-nia/components/CompInquiryPannel'
 
 const routeName = 'ServerMonitoring'
 
-const prefixUrl = 'http://incodej-lab.iptime.org:3000/d-solo'
 export default {
   name: routeName,
   // eslint-disable-next-line vue/no-unused-components
@@ -102,60 +101,85 @@ export default {
       name: routeName,
       src: `webpack:///${__filename.replace(/\\/g, '/').replace(/\?.*$/, '')}`,
       tempSrc: 'http://incodej-lab.iptime.org:3000/d-solo/XB7JYac7z/koren_data_all?orgId=1&panelId=24',
-      resource: [
-        { title: 'CPU 사용량', url: prefixUrl + '/LBlvJ7p7z/server_status?orgId=1&refresh=1m&panelId=14', class: 'w-50' },
-        { title: 'Memory 사용량', url: prefixUrl + '/LBlvJ7p7z/server_status?orgId=1&refresh=1m&panelId=12', class: 'w-50' },
-      ],
-      file3Hours1: [
-        { title: '장비', url: prefixUrl + '/88QEOB2nz/file_size?orgId=1&refresh=1m&panelId=22', class: 'w-50' },
-        { title: '경로', url: prefixUrl + '/88QEOB2nz/file_size?orgId=1&refresh=1m&panelId=21', class: 'w-50' },
-      ],
-      file3Hours2: [
-        { title: 'ipSdnSflow', url: prefixUrl + '/88QEOB2nz/file_size?orgId=1&refresh=1m&panelId=32', class: 'w-2/6' },
-        { title: 'ipSdnSyslog', url: prefixUrl + '/88QEOB2nz/file_size?orgId=1&refresh=1m&panelId=34', class: 'w-2/6' },
-        { title: 'ipSdnSyslogAlarm', url: prefixUrl + '/88QEOB2nz/file_size?orgId=1&refresh=1m&panelId=36', class: 'w-2/6' },
-      ],
-      fileDays: [
-        { title: 'link', url: prefixUrl + '/1edhLePnk/file_size_week?orgId=1&refresh=1h&panelId=18', class: 'w-2/6' },
-        { title: 'node', url: prefixUrl + '/1edhLePnk/file_size_week?orgId=1&refresh=1h&panelId=19', class: 'w-2/6' },
-        { title: 'interface', url: prefixUrl + '/1edhLePnk/file_size_week?orgId=1&refresh=1h&panelId=20', class: 'w-2/6' },
-      ],
-      fileData1: [
-        { title: '장비', url: prefixUrl + '/XB7JYac7z/koren_data_all?orgId=1&panelId=24', class: 'w-50' },
-        { title: '경로', url: prefixUrl + '/XB7JYac7z/koren_data_all?orgId=1&panelId=20', class: 'w-50' },
-      ],
-      fileData2: [
-        { title: 'ipSdnSflow', url: prefixUrl + '/XB7JYac7z/koren_data_all?orgId=1&panelId=25', class: 'w-2/6' },
-        { title: 'ipSdnSyslog', url: prefixUrl + '/XB7JYac7z/koren_data_all?orgId=1&panelId=21', class: 'w-2/6' },
-        { title: 'ipSdnSyslogAlarm', url: prefixUrl + '/XB7JYac7z/koren_data_all?orgId=1&panelId=26', class: 'w-2/6' },
-      ],
-      fileWrong1: [
-        { title: 'IP 시설 정보', url: prefixUrl + '/1edhLePnk/file_size_week?orgId=1&panelId=1', class: 'w-2/6' },
-        { title: '', url: prefixUrl + '/1edhLePnk/file_size_week?orgId=1&panelId=5', class: 'w-2/6' },
-        { title: '', url: prefixUrl + '/1edhLePnk/file_size_week?orgId=1&panelId=7', class: 'w-2/6' },
-      ],
-      fileWrong2: [
-        { title: '전송 시설 정보', url: prefixUrl + '/1edhLePnk/file_size_week?orgId=1&panelId=2' },
-        { title: '', url: prefixUrl + '/1edhLePnk/file_size_week?orgId=1&panelId=3' },
-        { title: '', url: prefixUrl + '/1edhLePnk/file_size_week?orgId=1&panelId=4' },
-        { title: '', url: prefixUrl + '/1edhLePnk/file_size_week?orgId=1&panelId=8' },
-        { title: '', url: prefixUrl + '/1edhLePnk/file_size_week?orgId=1&panelId=9' },
-        { title: '', url: prefixUrl + '/1edhLePnk/file_size_week?orgId=1&panelId=10' },
-      ],
-      fileWrong3: [
-        { title: 'Sflow 트래픽', url: prefixUrl + '/88QEOB2nz/file_size?orgId=1&panelId=12', class: 'w-2/6' },
-        { title: '광세기', url: prefixUrl + '/88QEOB2nz/file_size?orgId=1&panelId=6', class: 'w-2/6' },
-        { title: 'IP 트래픽', url: prefixUrl + '/88QEOB2nz/file_size?orgId=1&panelId=11', class: 'w-2/6' },
-      ],
-      collect: [
-        { title: '광세기', url: prefixUrl + '/XB7JYac7z/koren_data_all?orgId=1&panelId=14', class: 'w-100' },
-        { title: '트래픽', url: prefixUrl + '/XB7JYac7z/koren_data_all?orgId=1&panelId=16', class: 'w-100' },
-        { title: 'Sflow', url: prefixUrl + '/XB7JYac7z/koren_data_all?orgId=1&panelId=18', class: 'w-100' },
-      ],
+
     }
   },
-
-  computed: {},
+  computed: {
+    prefixUrl() {
+      const serverMonitorUrl = this.serverSetting.serverMonitorUrl ?? 'http://incodej-lab.iptime.org:3000'
+      return serverMonitorUrl + '/d-solo'
+    },
+    resource() {
+      return [
+        { title: 'CPU 사용량', url: this.prefixUrl + '/LBlvJ7p7z/server_status?orgId=1&refresh=1m&panelId=14', class: 'w-50' },
+        { title: 'Memory 사용량', url: this.prefixUrl + '/LBlvJ7p7z/server_status?orgId=1&refresh=1m&panelId=12', class: 'w-50' },
+      ]
+    },
+    file3Hours1() {
+ return [
+        { title: '장비', url: this.prefixUrl + '/88QEOB2nz/file_size?orgId=1&refresh=1m&panelId=22', class: 'w-50' },
+        { title: '경로', url: this.prefixUrl + '/88QEOB2nz/file_size?orgId=1&refresh=1m&panelId=21', class: 'w-50' },
+      ]
+    },
+    file3Hours2() {
+      return [
+      { title: 'ipSdnSflow', url: this.prefixUrl + '/88QEOB2nz/file_size?orgId=1&refresh=1m&panelId=32', class: 'w-2/6' },
+      { title: 'ipSdnSyslog', url: this.prefixUrl + '/88QEOB2nz/file_size?orgId=1&refresh=1m&panelId=34', class: 'w-2/6' },
+      { title: 'ipSdnSyslogAlarm', url: this.prefixUrl + '/88QEOB2nz/file_size?orgId=1&refresh=1m&panelId=36', class: 'w-2/6' },
+    ]
+},
+    fileDays() {
+      return [
+      { title: 'link', url: this.prefixUrl + '/1edhLePnk/file_size_week?orgId=1&refresh=1h&panelId=18', class: 'w-2/6' },
+      { title: 'node', url: this.prefixUrl + '/1edhLePnk/file_size_week?orgId=1&refresh=1h&panelId=19', class: 'w-2/6' },
+      { title: 'interface', url: this.prefixUrl + '/1edhLePnk/file_size_week?orgId=1&refresh=1h&panelId=20', class: 'w-2/6' },
+    ]
+},
+    fileData1() {
+      return [
+      { title: '장비', url: this.prefixUrl + '/XB7JYac7z/koren_data_all?orgId=1&panelId=24', class: 'w-50' },
+      { title: '경로', url: this.prefixUrl + '/XB7JYac7z/koren_data_all?orgId=1&panelId=20', class: 'w-50' },
+    ]
+},
+    fileData2() {
+      return [
+      { title: 'ipSdnSflow', url: this.prefixUrl + '/XB7JYac7z/koren_data_all?orgId=1&panelId=25', class: 'w-2/6' },
+      { title: 'ipSdnSyslog', url: this.prefixUrl + '/XB7JYac7z/koren_data_all?orgId=1&panelId=21', class: 'w-2/6' },
+      { title: 'ipSdnSyslogAlarm', url: this.prefixUrl + '/XB7JYac7z/koren_data_all?orgId=1&panelId=26', class: 'w-2/6' },
+    ]
+},
+    fileWrong1() {
+      return [
+      { title: 'IP 시설 정보', url: this.prefixUrl + '/1edhLePnk/file_size_week?orgId=1&panelId=1', class: 'w-2/6' },
+      { title: '', url: this.prefixUrl + '/1edhLePnk/file_size_week?orgId=1&panelId=5', class: 'w-2/6' },
+      { title: '', url: this.prefixUrl + '/1edhLePnk/file_size_week?orgId=1&panelId=7', class: 'w-2/6' },
+    ]
+},
+    fileWrong2() {
+      return [
+      { title: '전송 시설 정보', url: this.prefixUrl + '/1edhLePnk/file_size_week?orgId=1&panelId=2' },
+      { title: '', url: this.prefixUrl + '/1edhLePnk/file_size_week?orgId=1&panelId=3' },
+      { title: '', url: this.prefixUrl + '/1edhLePnk/file_size_week?orgId=1&panelId=4' },
+      { title: '', url: this.prefixUrl + '/1edhLePnk/file_size_week?orgId=1&panelId=8' },
+      { title: '', url: this.prefixUrl + '/1edhLePnk/file_size_week?orgId=1&panelId=9' },
+      { title: '', url: this.prefixUrl + '/1edhLePnk/file_size_week?orgId=1&panelId=10' },
+    ]
+},
+    fileWrong3() {
+      return [
+      { title: 'Sflow 트래픽', url: this.prefixUrl + '/88QEOB2nz/file_size?orgId=1&panelId=12', class: 'w-2/6' },
+      { title: '광세기', url: this.prefixUrl + '/88QEOB2nz/file_size?orgId=1&panelId=6', class: 'w-2/6' },
+      { title: 'IP 트래픽', url: this.prefixUrl + '/88QEOB2nz/file_size?orgId=1&panelId=11', class: 'w-2/6' },
+    ]
+},
+    collect() {
+        return [
+        { title: '광세기', url: this.prefixUrl + '/XB7JYac7z/koren_data_all?orgId=1&panelId=14', class: 'w-100' },
+        { title: '트래픽', url: this.prefixUrl + '/XB7JYac7z/koren_data_all?orgId=1&panelId=16', class: 'w-100' },
+        { title: 'Sflow', url: this.prefixUrl + '/XB7JYac7z/koren_data_all?orgId=1&panelId=18', class: 'w-100' },
+      ]
+    },
+  },
   mounted() {},
   methods: {
     reloadData(type) {
