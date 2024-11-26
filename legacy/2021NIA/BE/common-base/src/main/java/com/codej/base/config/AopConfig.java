5b884c5cf07f24de45f5fee0f60c9f2225fbd06b
dimmby <dimmbygo@gmail.com>
2024-11-26 16:17:21 +0900

    @EncryptResponse 정의

---
+++ legacy/2021NIA/BE/common-base/src/main/java/com/codej/base/config/AopConfig.java
@@ -0,0 +1,9 @@
+package com.codej.base.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@Configuration
+@EnableAspectJAutoProxy
+public class AopConfig {
+}
