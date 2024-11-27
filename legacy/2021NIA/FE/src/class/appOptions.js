3607ad39ea8268609c125e53bf3f4dfd8ebf1618
dimmby <dimmbygo@gmail.com>
2024-11-27 15:09:03 +0900

    dark theme 옵션 변경

--- legacy/2021NIA/FE/src/class/appOptions.js
+++ legacy/2021NIA/FE/src/class/appOptions.js
@@ -26,7 +26,7 @@ export class AppOptions extends Storage {
       baseURLs: this.readEnv(process.env.VUE_APP_BASE_API, ['//localhost:8070', '/mock']),
       baseURLIndex: this.readEnv(process.env.VUE_APP_BASE_API_INDEX, 0),
       isOnlyFront: this.readEnv(process.env.VUE_APP_ONLY_FE, false),
-      dark: false,
+      dark: this.readEnv(process.env.VUE_APP_USE_DARK_THEME, false),
       mobile: Device.instance.mobile ?? false,
       mock: null,
       projectList: ['datahub', 'demo', 'nia', 'ipms'],
