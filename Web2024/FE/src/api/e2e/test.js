7cc51d1c6913d6aaeaa1d7b3e46496d75177a492
dimmby <dimmbygo@gmail.com>
2024-12-18 17:12:48 +0900

    mock fileName 룰 변경

---
+++ Web2024/FE/src/api/e2e/test.js
@@ -0,0 +1,4 @@
+export const apis = [
+  '{ "url": "/selectOne/SELECT_AI_DETECTION_INFO", "sqlId": "SELECT_AI_DETECTION_INFO", "param": {"TICKET_ID":"1615707","FAULT_TIME":"2024-11-21 14:03:58","encrypt":"hH0+IIAGaIXXtfCSVIw="} }',
+]
+
