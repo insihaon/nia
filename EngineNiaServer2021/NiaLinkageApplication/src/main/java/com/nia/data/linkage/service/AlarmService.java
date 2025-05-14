630903a654cf1877535d4ad39a610fcbca8bbe44
gosungho <dloere3@naver.com>
2025-05-14 16:51:51 +0900

    NiaLinkageApplication 모듈 - 로그 수정 해서 배포

--- EngineNiaServer2021/NiaLinkageApplication/src/main/java/com/nia/data/linkage/service/AlarmService.java
+++ EngineNiaServer2021/NiaLinkageApplication/src/main/java/com/nia/data/linkage/service/AlarmService.java
@@ -1,12 +1,15 @@
 package com.nia.data.linkage.service;
 
-import com.nia.data.linkage.vo.alarm.json.AlarmHitsDataVo;
+import com.nia.data.linkage.vo.alarm.json.AlarmHitsHitsDataVo;
+import com.nia.data.linkage.vo.alarm.json.AlarmHitsVo;
+
+import java.util.List;
 
 public interface AlarmService {
 
     void getAlarmData();
 
-    void setAlarmData(AlarmHitsDataVo alarmHitsDataVo);
+    void setAlarmData(List<AlarmHitsVo> histsList);
 
     String setAlarmLvl(String level);
 }
