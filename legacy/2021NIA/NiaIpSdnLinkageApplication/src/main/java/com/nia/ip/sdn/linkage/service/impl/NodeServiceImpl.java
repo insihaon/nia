508709a7b73924601e6ea7b0360f73221755780e
gosungho <dloere3@naver.com>
2025-12-12 15:49:02 +0900

    신규 AIB 이상트래픽 TCA 발행을 위해 NIA.TB_ATT_AI_TCA_MODEL에 데이터 추가하도록 개발

--- legacy/2021NIA/NiaIpSdnLinkageApplication/src/main/java/com/nia/ip/sdn/linkage/service/impl/NodeServiceImpl.java
+++ legacy/2021NIA/NiaIpSdnLinkageApplication/src/main/java/com/nia/ip/sdn/linkage/service/impl/NodeServiceImpl.java
@@ -24,6 +24,7 @@ public class NodeServiceImpl implements NodeService {
 
     @Autowired
     private NiaNodeMapper niaNodeMapper;
+
     @Autowired
     private NiaLinkageHistMapper niaLinkageHistMapper;
 
@@ -50,7 +51,10 @@ public class NodeServiceImpl implements NodeService {
 
                 if(insertCnt > 0){
                     niaNodeMapper.deleteNode();
+                    niaNodeMapper.deleteAttAiTcaModel();
+
                     niaNodeMapper.insertNode();
+                    niaNodeMapper.insertAttAiTcaModel();
 
                     strHashMap = new HashMap<>();
                     strHashMap.put("key", "ipSdnNodeKey");
