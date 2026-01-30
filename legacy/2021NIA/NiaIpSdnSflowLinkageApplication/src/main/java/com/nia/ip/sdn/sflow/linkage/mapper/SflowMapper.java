a53eea3cc6a903523b47a409fbf4b2cd7034fd9a
gosungho <dloere3@naver.com>
2026-01-30 17:12:54 +0900

    sflow Batch 단위로 수집되도록 변경경

--- legacy/2021NIA/NiaIpSdnSflowLinkageApplication/src/main/java/com/nia/ip/sdn/sflow/linkage/mapper/SflowMapper.java
+++ legacy/2021NIA/NiaIpSdnSflowLinkageApplication/src/main/java/com/nia/ip/sdn/sflow/linkage/mapper/SflowMapper.java
@@ -5,10 +5,11 @@ import com.nia.ip.sdn.sflow.linkage.vo.sflow.SflowDataVo;
 import org.apache.ibatis.annotations.Mapper;
 
 import java.util.HashMap;
+import java.util.List;
 
 @Mapper
 public interface SflowMapper {
-    long selectSflowSeq();
-    void insertSflowData(SflowCollectVo sflowCollectVo);
+    long selectCurrentSflowSeq();
     void updateLinkageHist(HashMap<String, String> hashMap);
+    int insertSflowDataBatch(List<SflowCollectVo> list);
 }
