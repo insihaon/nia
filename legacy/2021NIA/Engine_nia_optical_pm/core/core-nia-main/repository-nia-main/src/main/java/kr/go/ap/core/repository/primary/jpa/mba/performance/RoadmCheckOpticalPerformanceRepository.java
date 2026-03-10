bff57fe6105e50d04d110dafbb05ffa778b38739
gosungho <dloere3@naver.com>
2026-03-10 15:11:31 +0900

    Prepare B project for subtree integration

--- legacy/2021NIA/Engine_nia_optical_pm/core/core-nia-main/repository-nia-main/src/main/java/kr/go/ap/core/repository/primary/jpa/mba/performance/RoadmCheckOpticalPerformanceRepository.java
+++
@@ -1,8 +0,0 @@
-package kr.go.ap.core.repository.primary.jpa.mba.performance;
-
-import kr.go.ap.core.primary.nia.entity.mba.performance.RoadmCheckOpticalPerformanceEntity;
-import kr.go.ap.core.primary.nia.entity.mba.performance.key.RoadmCheckOpticalPerformanceKey;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoadmCheckOpticalPerformanceRepository extends JpaRepository<RoadmCheckOpticalPerformanceEntity, RoadmCheckOpticalPerformanceKey> {
-}
--- legacy/2021NIA/Engine_nia_optical_pm/core/core-nia-main/repository-nia-main/src/main/java/kr/go/ap/core/repository/primary/jpa/mba/ticket/RoadmLowOpticalPerformanceRepository.java
+++
@@ -1,8 +0,0 @@
-package kr.go.ap.core.repository.primary.jpa.mba.ticket;
-
-import kr.go.ap.core.primary.nia.entity.mba.ticket.RoadmLowOpticalPerformanceEntity;
-import kr.go.ap.core.primary.nia.entity.mba.ticket.key.RoadmLowOpticalPerformanceKey;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoadmLowOpticalPerformanceRepository extends JpaRepository<RoadmLowOpticalPerformanceEntity, RoadmLowOpticalPerformanceKey> {
-}
